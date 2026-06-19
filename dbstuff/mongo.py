c89005f3ec49ca6acbbb86c3daf2cf08b9f3b1d2
Yuvaansh Kapila <yuvaanshkapila@yahoo.com>
2026-06-19 02:07:41 -0400

    tweak mongo

--- dbstuff/mongo.py
+++ dbstuff/mongo.py
@@ -1,16 +1,17 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-
+#find the .env file
 envPath = os.path.join(os.path.dirname(__file__), "..", ".env")
 load_dotenv(envPath)
-
+#connect to the mongo cluster
 client = MongoClient(os.environ["MONGO_URI"])
 
 db = client["todo"]
 tasks = db["tasks"]
 
 tasks.insert_one({"text": "buy milk", "status": "open"})
+#grab every task back and print it
 
 for task in tasks.find():
     print(task)
